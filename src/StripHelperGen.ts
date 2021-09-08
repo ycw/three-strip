32ff2c75e89f01d5d3c4c56089c196cd64cd6e93
ycw <ycw7788@gmail.com>
2021-09-08 17:08:53 +0800

    proper dispose

--- src/StripHelperGen.ts
+++ src/StripHelperGen.ts
@@ -2,8 +2,14 @@ import type * as THREE from 'three'
 import type { Strip } from './Strip'
 import * as Err from './Err'
 
-// A fn generates a StripHelper class
+/**
+ * A fn to gen StripHelper class
+ */
 export function StripHelperGen($: typeof THREE) {
+
+  /**
+   * Display TBNs for given strip.
+   */
   return class StripHelper extends $.LineSegments {
 
     #strip: Strip;
@@ -104,17 +110,25 @@ export function StripHelperGen($: typeof THREE) {
       // guard
 
       if (!this.#strip.geometry || !this.#strip.frames) {
+        this.geometry.deleteAttribute('color');
+        this.geometry.deleteAttribute('position');
         return;
       }
+
+      // cache
 
       const $frms = this.#strip.frames;
       const $I = $frms.length;
 
-      const aColor = new $.Float32BufferAttribute($I * 18, 3)
+      // re alloc
+
+      const aColor = new $.Float32BufferAttribute($I * 18, 3);
       this.geometry.setAttribute('color', aColor);
 
       const aPo = new $.Float32BufferAttribute($I * 18, 3);
       this.geometry.setAttribute('position', aPo);
+
+      // cache
 
       const $v0 = new $.Vector3();
       const $v1 = new $.Vector3();
@@ -123,6 +137,8 @@ export function StripHelperGen($: typeof THREE) {
       const $aPo = this.#strip.geometry.attributes.position;
 
       for (let i = 0, $i = -1; i < $I; ++i) {
+
+        // find pos ( mid of 2 handles )
 
         $v0.set(
           $aPo.getX($i = i * 2),
@@ -136,11 +152,11 @@ export function StripHelperGen($: typeof THREE) {
           $aPo.getZ($i)
         );
 
-        $v3.addVectors($v0, $v1).multiplyScalar(.5);
+        $v3.addVectors($v0, $v1).multiplyScalar(.5); // =sample point, p
 
-        $v0.copy($frms[i][1]).multiplyScalar(this.#len).add($v3); // B
-        $v1.copy($frms[i][2]).multiplyScalar(this.#len).add($v3); // N
-        $v2.copy($frms[i][0]).multiplyScalar(this.#len).add($v3); // T
+        $v0.copy($frms[i][1]).multiplyScalar(this.#len).add($v3); // B for x
+        $v1.copy($frms[i][2]).multiplyScalar(this.#len).add($v3); // N for y
+        $v2.copy($frms[i][0]).multiplyScalar(this.#len).add($v3); // T for z
 
         (aPo.array as Float32Array).set([
           $v3.x, $v3.y, $v3.z, $v0.x, $v0.y, $v0.z, // p->B
@@ -159,6 +175,16 @@ export function StripHelperGen($: typeof THREE) {
           this.#c2.r, this.#c2.g, this.#c2.b
         ], i * 18);
       }
+    }
+
+    /**
+     * Dispose internal geometry and material 
+     */
+    dispose() {
+      this.update();
+      Array.isArray(this.material)
+        ? this.material.forEach(m => m.dispose())
+        : this.material.dispose();
     }
   }
 }
