ebfaff0790a6f723f8580786ab8699e96af2dd06
ycw <ycw7788@gmail.com>
2021-09-20 01:41:23 +0800

    rebuild

--- build/three-strip.d.ts
+++ build/three-strip.d.ts
@@ -2,12 +2,13 @@ import * as THREE from 'three';
 
 declare type RadiusFn = (i: number, I: number) => number;
 declare type TiltFn = (i: number, I: number) => number;
-declare type UvFn = (i: number, I: number) => [
+declare type UvFn = (i: number, I: number, // dash-space
+j: number, J: number) => ([
     number,
     number,
     number,
     number
-];
+]);
 declare type Curve = THREE.Curve<THREE.Vector3> | THREE.CurvePath<THREE.Vector3>;
 declare type Frame = [
     THREE.Vector3,
@@ -15,30 +16,35 @@ declare type Frame = [
     THREE.Vector3,
     THREE.Vector3
 ];
+declare type Segments = [number] | [number, number[]] | [number, number[], number];
+declare type ParsedSegments = [
+    number,
+    number[],
+    number
+];
 
 declare class Strip {
     curve: Curve;
     radius: number | RadiusFn;
     tilt: number | TiltFn;
-    static UvPresets: [UvFn, UvFn, UvFn, UvFn];
     constructor(curve: Curve, radius?: number | RadiusFn, tilt?: number | TiltFn);
     computeFrames(segments: number): Frame[];
 }
 
-declare type Segments = number | [number] | [number, number] | [number, number, number];
 declare class StripGeometry extends THREE.BufferGeometry {
     #private;
-    constructor(strip: Strip, segments: Segments, uvFn?: UvFn);
+    constructor(strip: Strip, segments: number | Segments, uvFn?: UvFn);
+    static parseSegments(segments: number | Segments): ParsedSegments;
 }
 
 declare class StripHelper extends THREE.LineSegments {
     strip: Strip;
     segments: number;
-    length: number;
+    size: number;
     xColor: THREE.Color;
     yColor: THREE.Color;
     zColor: THREE.Color;
-    constructor(strip: Strip, segments: number, length?: number, xColor?: THREE.ColorRepresentation, yColor?: THREE.ColorRepresentation, zColor?: THREE.ColorRepresentation);
+    constructor(strip: Strip, segments: number, size?: number, xColor?: THREE.ColorRepresentation, yColor?: THREE.ColorRepresentation, zColor?: THREE.ColorRepresentation);
     update(): void;
 }
 
