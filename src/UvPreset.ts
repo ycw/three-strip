fcc97f371def4ea4831e38c5c028017bbfc602ce
ycw <ycw7788@gmail.com>
2021-09-20 01:18:59 +0800

    add `UvPreset` class

---
+++ src/UvPreset.ts
@@ -0,0 +1,28 @@
+import { UvFn } from "./Type";
+
+function uvFnGen(
+  x: number,
+  mode: 0 | 1 | 2 | 3
+): ReturnType<UvFn> {
+  switch (mode) {
+    case 0: return [0, x, 1, x];
+    case 1: return [x, 1, x, 0];
+    case 2: return [1, x, 0, x];
+    case 3: return [x, 0, x, 1];
+  }
+}
+
+export class UvPreset {
+  static dash: [UvFn, UvFn, UvFn, UvFn] = [
+    (i, I) => uvFnGen(i / I, 0),
+    (i, I) => uvFnGen(i / I, 1),
+    (i, I) => uvFnGen(i / I, 2),
+    (i, I) => uvFnGen(i / I, 3),
+  ];
+  static strip: [UvFn, UvFn, UvFn, UvFn] = [
+    (_i, _I, j, J) => uvFnGen(j / J, 0),
+    (_i, _I, j, J) => uvFnGen(j / J, 1),
+    (_i, _I, j, J) => uvFnGen(j / J, 2),
+    (_i, _I, j, J) => uvFnGen(j / J, 3),
+  ];
+}
